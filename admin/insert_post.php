e3bb317b6e180620c13806806cfd61348fcc6198
drpatpat <drpatpat@live.com>
2015-03-23 11:38:15 +0000

    Insert post to database

---
+++ admin/insert_post.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        <form action="insert_post.php" method="post" enctype="multipart/form-data">
+            <table width="800" align="center" border="2">
+                <tr>
+                    <td colspan="6"><h1>Insert New Post:</h1></td>
+                </tr>
+                <tr>
+                    <td align="right"><strong>Post Title:</strong></td>
+                    <td><input type="text" name="post_title"/></td>
+                </tr>
+                <tr>
+                    <td align="right"><strong>Post Category:</strong></td>
+                    <td><input type="text" name="post_title"/></td>
+                </tr>
+                <tr>
+                    <td align="right"><strong>Post Author:</strong></td>
+                    <td><input type="text" name="post_title"/></td>
+                </tr>
+                <tr>
+                    <td align="right"><strong>Post Keywords:</strong></td>
+                    <td><input type="text" name="post_title"/></td>
+                </tr>
+                <tr>
+                    <td align="right"><strong>Post Image:</strong></td>
+                    <td><input type="text" name="post_title"/></td>
+                </tr>
+                <tr>
+                    <td align="right"><strong>Post Content:</strong></td>
+                    <td><input type="text" name="post_title"/></td>
+                </tr>
+                <tr>
+                    <td colspan="6" align="center"><input type="submit" name="submit" value="Publish Now" /></td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -3,8 +3,42 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>A News Platform</title>
+        <link rel="stylesheet" href="styles/style.css" type="text/css" />
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <div class="container">
+            <div class="head">
+                <img id="logo" src="images/logo.png" />
+                <img id="banner" src="images/ad_banner.png" />
+            </div>
+            
+            <div class="navbar">
+                <ul id="menu">
+                    <?php
+                        include("includes/connect.php");
+                    
+                        $get_cats = "SELECT * FROM categories";
+                        $run_cats = $db->query($get_cats);
+                        
+                        while($cats_row = $run_cats->fetch_array()) {
+                            $cat_id = $cats_row['cat_id'];
+                            $cat_title = $cats_row['cat_title'];
+                            echo "<li><a href='index.php?cat=$cat_id'>$cat_title</a></li>";
+                        }
+                    ?>
+                </ul>
+                
+                <div>
+                    <form id="navbar-form" method="get" action="results.php" enctype="multipart/form-data">
+                        <input type="text" name="search_query" />
+                        <input type="submit" name="search" value="Search Now" />
+                    </form>
+                </div>
+            </div>
+            
+            <div class="sidebar">This is the sidebar</div>
+            <div class="post_area">This is the body</div>
+            <div class="footer_area">This is the footer</div>
+        </div>
     </body>
 </html>
